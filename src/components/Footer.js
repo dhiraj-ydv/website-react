787a350ba5b9ba0a199cfc2f251736c66b3c7ec9
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-06-25 13:53:50 +0530

    changes in socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,13 @@ const Footer = () => (
   <footer className="bg-gray-800 text-white py-4">
     <div className="container mx-auto text-center">
       <div className="social-links mb-4">
-        <a href="https://twitter.com/yourprofile" className="mx-2 text-white hover:text-gray-400">
+        <a href="https://twitter.com/dhirajY08" className="mx-2 text-white hover:text-gray-400">
           <i className="fab fa-twitter fa-2x"></i>
         </a>
-        <a href="https://linkedin.com/in/yourprofile" className="mx-2 text-white hover:text-gray-400">
+        <a href="https://linkedin.com/in/dhirajyadav08" className="mx-2 text-white hover:text-gray-400">
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
-        <a href="https://github.com/yourprofile" className="mx-2 text-white hover:text-gray-400">
+        <a href="https://github.com/dhiraj-ydv" className="mx-2 text-white hover:text-gray-400">
           <i className="fab fa-github fa-2x"></i>
         </a>
       </div>
