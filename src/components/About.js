6bee609623db77b4d956a84b7cec0e49b0abd301
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-07-16 12:57:51 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ const About = () => (
       <h3 className="section-header">Hi, I'm Dhiraj Yadav!</h3>
       <p className="section-content">
         I'm enthusiastic about learning and discussing technology, polity and governance, 
-        cosmology, sociology, and qualitative fundamental analysis. I also enjoy gymming, playing AirBadminton, and playing tactical FPS games. Feel free to explore my blog for more insights.
+        cosmology, sociology, and qualitative fundamental analysis. I also enjoy Gymming, playing AirBadminton, and playing Tactical FPS Games. Feel free to explore my blog for more insights.
       </p>
       <div>
         <h4 className="section-subheader">EDUCATION</h4>
